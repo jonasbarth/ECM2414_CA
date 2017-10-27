aae30b2ee72e3b45a284b05471ed5defb6340b26
jonasbarth <jb901@exeter.ac.uk>
2017-10-27 12:23:39 +0100

    Uploaded class files

---
+++ WhiteBag.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ECM2414_CA;
+
+/**
+ *
+ * @author Jonas
+ */
+public class WhiteBag extends Bag{
+    
+}
