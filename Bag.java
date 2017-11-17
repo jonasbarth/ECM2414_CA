d6cd253231d948813d9703c5ff2a82fa0345b92d
theghia <theghiactr@gmail.com>
2017-11-17 13:28:04 +0000

    Update Bag.java
    Completed the  Java Doc, a few comments for clarity and deleted some bloated code.

--- Bag.java
+++ Bag.java
@@ -15,11 +15,10 @@ import java.util.Random;
 
 /**
  *
- * @author 
+ * @author 660050748, 660049985
  */
 public abstract class Bag {
     
-    //private Pebble[] pebbles;
     private Bag partnerBag;
     private ArrayList pebbles;
     private Random random;
@@ -27,9 +26,11 @@ public abstract class Bag {
     
     
     /**
-     * 
-     * @param pebbles
-     * @param partnerBag 
+     * Constructor for bags when other bag objects have already been instantiated 
+     *
+     * @param pebbles, the pebbles that the new bag will hold
+     * @param partnerBag, the bag that is directly linked to a particular Bag object
+     * @param name of the bag
      */
     public Bag(Pebble[] pebbles, Bag partnerBag, String name) {
         this.pebbles = new ArrayList(1);
@@ -37,90 +38,82 @@ public abstract class Bag {
             this.pebbles.add(pebble);
         }
         setPartnerBag(partnerBag);
-        Random random = new Random();
-        this.random = random;
+        this.random = new Random();
         
         this.name = name;
     }
     
+     /**
+     * Constructor for bags when other bag objects have not already been instantiated 
+     *
+     * @param partnerBag, the bag that is directly linked to a particular Bag object
+     * @param name of the bag
+     */
     public Bag(Pebble[] pebbles, String name) {
         this.pebbles = new ArrayList(1);
         for (Pebble pebble : pebbles) {
             this.pebbles.add(pebble);
         }
-        //System.out.println("Pebble constructor " + this.pebbles.toString());
-        //this.pebbles.trimToSize();
-        
         this.name = name;
         this.random = new Random();
     }
     
-    public Bag(String name) {
-        this.pebbles = new ArrayList(1);
-        this.name = name;
-        this.random = new Random();
-    }
-    
+     /**
+     * Method to get the name of the Bag object
+     *
+     * @return the name of the Bag which will be of type String
+     */
     public String getName() {
         return this.name;
     }
     
     /**
-     * Method to set partnerbag
-     * @param partnerBag 
+     * Method to set the partnerbag for the current Bag object
+     *
+     * @param partnerBag which is of type Bag
      */
     public void setPartnerBag(Bag partnerBag) {
         this.partnerBag = partnerBag;
     }
     
     /**
-     * Method to get partner bag
-     * @return Bag 
+     * Method to get the partner bag of the current Bag object
+     *
+     * @return the Bag object that is associated with the current Bag object
      */
     public Bag getPartnerBag() {
         return this.partnerBag;
     }
     
     /**
-     * Method returning array holding pebbles
-     * @return Pebble[]
+     * Method to convert the ArrayList containing Pebbles into a Pebble[] and returning
+     * that new array which contains the pebbles currently in the Bag object
+     * 
+     * @return Pebble[] the array of pebbles currently in the Bag object
      */
     public Pebble[] getAllPebbles() {
-        //this.pebbles.trimToSize();
+        //Due to the nature of ArrayList, if there are less than 10 objects 
+        //instantiated with it. The rest will be made up of Null references
         removeNulls();
         Object[] object = this.pebbles.toArray();
-        object = takeNullAway(object);
-        Pebble[] pebbles = new Pebble[object.length]; //pebble array to hold pebbles from ArrayList
+        //pebble array to hold pebbles from ArrayList
+        Pebble[] pebbles = new Pebble[object.length]; 
         
         for (int i = 0; i < object.length; i++) {
             pebbles[i] = (Pebble) object[i];
         }
         return pebbles;
     }
-
-    private Object[] takeNullAway(Object[] object) {
-        int count = object.length;
-        for (Object o : object) {
-            if (o == null) {
-                count--;
-            }
-        }
-        Object[] obj = new Object[count];
-        for (int i = 0; i < count; i++) {
-            obj[i] = object[i];
-        }
-        return obj;
-        
-    }
     
+    //Service method to remove any possible null values in the field pebbles, type ArrayList
     private void removeNulls() {
         this.pebbles.removeAll(Collections.singleton(null));
     }
     
     
     /**
-     * Method to add a certain pebble to bag
-     * @param pebble 
+     * Method to add a certain pebble to this bag
+     * @param pebble to be added to bag because we are discarding to it
      */
     public synchronized void addPebble(Pebble pebble) {
         this.pebbles.add(pebble);
@@ -129,26 +122,27 @@ public abstract class Bag {
     
     public synchronized void addPebble(Pebble[] pebbles){
         this.pebbles.addAll(Arrays.asList(pebbles));
-        this.pebbles.trimToSize();
+        //I took out trim to size -- if it breaks add it here again
     }
     
-    /**
-     * Method to remove a pebble from bag
-     * @param pebble 
-     */
-    public synchronized void removePebble(Pebble pebble) {
-        
+    //Service method to remove a particular pebble from this current bag
+    // -- converted to private, if it breaks, change this up
+    private synchronized void removePebble(Pebble pebble) { 
         this.pebbles.remove(pebble);
         this.pebbles.trimToSize();
     }
     
+    /**
+     * Method to remove all pebbles from this current bag
+     */
     public synchronized void removeAllPebbles() {
         this.pebbles.clear();
     }
     
     /**
-     * Returns the value of all pebbles in the bag as a string
-     * @return String
+     * Returns the value of all pebbles in the bag as a string. Will
+     * be used to display the current pebbles in the bag
+     * @return String type that have the values of of the pebbles currently in the bag
      */
     public String getPebblesAsString() {
         return Arrays.toString(getIntPebbleArray());
@@ -163,29 +157,20 @@ public abstract class Bag {
     public synchronized Pebble getRandomPebble(){
         removeNulls();
         Object[] temp = this.pebbles.toArray();
+        int randNum = this.random.nextInt(temp.length);
+        Pebble pebbleToBeChosen = (Pebble) temp[randNum];
         
-        //System.out.println(this.random);
-        int randNum = this.random.nextInt(temp.length); //+ 1;
-        
-        Pebble temp2 = (Pebble) temp[randNum];
-        
-        this.removePebble(temp2);
-        return temp2;   
+        this.removePebble(pebbleToBeChosen);
+        return pebbleToBeChosen;   
     }
-    
-    
-    /**
-     * Service method to return value of all pebbles in the bag in an int array
-     * @return 
-     */
+  
+    // Service method to return value of all pebbles in the bag in an int array 
     private int[] getIntPebbleArray() {
         removeNulls();
         Object[] object = this.pebbles.toArray();
-        Object[] obj = takeNullAway(object);
         int[] ints = new int[obj.length]; //int array to hold value of each pebble
         
         for (int i = 0; i < obj.length; i++) {
-            //System.out.println(obj[i]);
             Pebble pebble = (Pebble) obj[i];
             ints[i] = pebble.getValue();
         }
