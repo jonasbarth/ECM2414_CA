d2b8391239337ef6a80ac7d04392e997b66f16e9
theghia <theghiactr@gmail.com>
2017-11-17 14:39:08 +0000

    Update IllegalPebbleValueException.java
    Java Doc and changing the name

--- IllegalPebbleValueException.java
+++ IllegalPebbleValueException.java
@@ -8,7 +8,7 @@ package ECM2414_CA;
 /**
  * Class for the Exceptions, specifically for when an illegal value of the weight of the pebble has been found.
  *
- * @author Jonas
+ * @author 660050748, 660049985
  */
 public class IllegalPebbleValueException extends Exception {
     
