911e30af1cab228194a0b6908919b7531ddc1884
theghia <theghiactr@gmail.com>
2017-11-17 13:39:56 +0000

    Update WhiteBag.java
    Added to the Java Doc for the explanation of the class and deleted some more bloated code

--- WhiteBag.java
+++ WhiteBag.java
@@ -6,8 +6,10 @@
 package ECM2414_CA;
 
 /**
- *
- * @author Jonas
+ * Class to allow the creation of Bag type object as Bag is abstract. Allows for distinction
+ * between the different coloured Bag objects since the WhiteBag does not add any more functionality
+ * as specified in the super class.
+ * @author 660050748, 660049985
  */
 public class WhiteBag extends Bag{
     
@@ -17,12 +19,5 @@ public class WhiteBag extends Bag{
     
     public WhiteBag(Pebble[] pebbles, String name) {
         super(pebbles, name);
-    }
-    /*
-    public WhiteBag(String name) {
-        Pebble[] pebbles = new Pebble[0];
-        super(pebbles, name);
-    }*/
-    
-   
+    }   
 }
