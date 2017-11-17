cba46d26e9e08c949beb218107866b7aea5d7ec0
theghia <theghiactr@gmail.com>
2017-11-17 14:15:38 +0000

    Update IllegalFileContentException.java
    Java doc

--- IllegalFileContentException.java
+++ IllegalFileContentException.java
@@ -6,8 +6,10 @@
 package ECM2414_CA;
 
 /**
+ * Class for Exceptions that occur when the content of the file is not of the specified format: A list of comma, seperated
+ * integers 
  *
- * @author Jonas
+ * @author 660050748, 660049985
  */
 class IllegalFileContentException extends Exception {
     public IllegalFileContentException(String message) {
