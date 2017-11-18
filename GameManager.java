b3edbaead7c1d27ab146b67d1196d80e925437a6
jonasbarth <32641360+jonasbarth@users.noreply.github.com>
2017-11-18 19:56:58 +0000

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -8,13 +8,21 @@ package ECM2414_CA;
 import ECM2414_CA.listenerEvent.GameListener;
 
 /**
- *
- * @author jb901
+ * Interface to be implemented by the PebbleGame class
+ * @author 660050748, 660049985
  */
 public interface GameManager {
     
+    /**
+     * Method to register an array of GameListeners
+     * 
+     */
     public void registerGameListeners(GameListener[] listeners);
     
+    /**
+     * Method to register a single GameListener
+     * 
+     */
     public void registerGameListener(GameListener listener);
     
     
