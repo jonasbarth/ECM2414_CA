4770b466a8ead3603f40fb63e1ec9b550945522a
theghia <theghiactr@gmail.com>
2017-11-18 16:11:30 +0000

    Update WriteToFile.java
    Comments to explain code and doc string

--- WriteToFile.java
+++ WriteToFile.java
@@ -10,23 +10,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Write a description of class Source here.
+ * Class containing methods to create a file and to write to it
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author 660050748, 660049985
+ * @version 18/11/2017
  */
 public class WriteToFile
 {
     private String filepath;
     
+    /**
+    * Constructor for objects of class WriteToFile
+    *
+    * @param filepath String that will have the path of the file on the system
+    */
     public WriteToFile(String filepath) throws IOException {
         this.filepath = filepath;
         createFile(this.filepath);
     }
-    
+    //Helper method used to create the necessary files
     private void createFile(String filepath) throws IOException {
         
         File file = new File(filepath);
+        //if the file  already exists with that path name, then we will delete 
+        //it and create a new one with the same pathname. Essentially over writing.
+        //this happens because as we keep on playing more games, the files being created
+        //will have the same name as the ones already created.
         if (file.isFile()) {
             file.delete();
             file.createNewFile();
@@ -37,6 +46,13 @@ public class WriteToFile
         
     }
     
+    /**
+    * Method writes the String argument to the path of the file that the 
+    * WriteToFile Object contains
+    *
+    * @param textLine String that is the text you want to write to the file
+    * @throws IOException 
+    */
     public void writeLineIntoFile(String textLine) throws IOException {
         textLine += "\n";
         FileWriter write = new FileWriter(this.filepath, true);
