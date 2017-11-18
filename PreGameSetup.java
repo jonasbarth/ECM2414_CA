7227801a58e7add486eeb5e86a9ccc958695b9cd
theghia <theghiactr@gmail.com>
2017-11-18 20:00:58 +0000

    Update PreGameSetup.java
    Just some indentations because I am OCD and finished the Doc String

--- PreGameSetup.java
+++ PreGameSetup.java
@@ -30,12 +30,16 @@ public class PreGameSetup {
         vf = new VerifyInput();
         this.bagValues = new int[3][];
     }
-
-public void askInput() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Press E to exit");
-    boolean exit = false;
-    while (!exit) {
+    
+    /**
+    * Method gets the number of players and the specific files needed to initiate the PebbleGame.
+    * Otherwise, the method just exits and the game ceases.
+    */
+    public void askInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press E to exit");
+        boolean exit = false;
+        while (!exit) {
             String next = null;
             next = getInfoOnNumOfPlayers(scanner);
             if (next.equals("E")) {
@@ -62,63 +66,63 @@ public void askInput() {
         
     }    
     
-//Helper method that will return a String that will either be 'E' (to exit) or the path to get the file
-private String getInfoOnPathForPebbles(Scanner scanner) {
-    String next = null;
-    String[] counts = {"first", "second", "third"};
-    for (int i = 0; i < 3; i++) {
-         System.out.println("Please give the file location of the values for the " + counts[i] + " black bag");
-         while (true) {
+    //Helper method that will return a String that will either be 'E' (to exit) or the path to get the file
+    private String getInfoOnPathForPebbles(Scanner scanner) {
+        String next = null;
+        String[] counts = {"first", "second", "third"};
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Please give the file location of the values for the " + counts[i] + " black bag");
+            while (true) {
+                next = scanner.nextLine();
+                try {
+                    if (next.equals("E") || validFile(next)) break;
+                        } catch (Exception e) {
+                            System.out.println(e);
+                            }
+                }
+            if (next.equals("E")) break;
+            }
+        return next;    
+
+    //helper method that will return a String 'E' (to exit) or a number to determine the player amount    
+    private String getInfoOnNumOfPlayers(Scanner scanner) {
+        System.out.println("Please enter the number of players...");
+        String next = "";
+        while (true) {
             next = scanner.nextLine();
+            if (validPlayerEntry(next) || next.equals("E")) break;
+            System.out.println("Wrong input, please give a valid player entry");
+                }
+        return next;
+        }    
+
+    //Helper method to verify that amount of players inputted is greater than 1    
+    private boolean validPlayerEntry(String entry) {
             try {
-                if (next.equals("E") || validFile(next)) break;
-                    } catch (Exception e) {
-                        System.out.println(e);
-                        }
-             }
-         if (next.equals("E")) break;
-         }
-     return next;    
-
-//helper method that will return a String 'E' (to exit) or a number to determine the player amount    
-private String getInfoOnNumOfPlayers(Scanner scanner) {
-    System.out.println("Please enter the number of players...");
-    String next = "";
-    while (true) {
-        next = scanner.nextLine();
-        if (validPlayerEntry(next) || next.equals("E")) break;
-        System.out.println("Wrong input, please give a valid player entry");
+                int players = Integer.parseInt(entry);
+                if (players < 2) {
+                    return false;
+                }
+                this.playerEntry = players;
+                this.vf.setPlayerNumber(players);
+                return true;
             }
-    return next;
-    }    
-
-//Helper method to verify that amount of players inputted is greater than 1    
-private boolean validPlayerEntry(String entry) {
-        try {
-            int players = Integer.parseInt(entry);
-            if (players < 2) {
+            catch (NumberFormatException e) {
                 return false;
-            }
-            this.playerEntry = players;
-            this.vf.setPlayerNumber(players);
-            return true;
-        }
-        catch (NumberFormatException e) {
-            return false;
         
+            }
         }
-    }
 
-//Helper method to determine if the path for the file is valid
-private boolean validFile(String filepath) throws IOException, NoFileExtentionException, InvalidFileExtentionException, IllegalFileContentException, FileNotFoundException {
-        if (vf.verifyFileMeta(filepath) && vf.verifyFileContent(filepath)) {
-            System.out.println("File content accepted");
-            return true;
+    //Helper method to determine if the path for the file is valid
+    private boolean validFile(String filepath) throws IOException, NoFileExtentionException, InvalidFileExtentionException, IllegalFileContentException, FileNotFoundException {
+            if (vf.verifyFileMeta(filepath) && vf.verifyFileContent(filepath)) {
+                System.out.println("File content accepted");
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         PreGameSetup ask = new PreGameSetup();
         
         ask.askInput();
